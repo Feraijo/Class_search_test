6f1c27660837d146f265f2706b4dd4425852c6b0
Feraijo <Feraijo@gmail.com>
2016-10-05 19:23:13 +0500

    Some refactoring

--- src/main/Searcher.java
+++ src/main/Searcher.java
@@ -2,9 +2,20 @@ package main;
 
 import java.util.*;
 
-/**
- * Created by Feraijo on 23.09.2016.
- */
+    /**
+     "Imagine that you - the developer from the ancient times, when IDE were only start to exist.
+     You have big project in Java with hundreds of thousands files. To get your life more easy,
+     you decided to implement the class search function by its name. For convenience, you enter
+     only the first letters of the name of the class, IDE gives you a list of 12 classes that start
+     with the characters entered. In the list classes are sorted by last modification (recently
+     saved at the beginning), if changed at the same time - sorted lexicographically.
+
+     It is expected that when the project is opened, data is indexed one time, then searches are
+     performed quickly. That class search function will be used not only by yourself."
+
+     Decision must have a class with a default constructor that implements the interface ISearcher.
+     * Created by Feraijo on 23.09.2016.
+     */
 class Searcher implements ISearcher {
     private SortedMap<String, Long> classes = new TreeMap<>();
 
